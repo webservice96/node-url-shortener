54ad4ce39cecdf5d0ffe447279d1f467b9d2475f
webservice96 <dmdhelal@gmail.com>
2021-09-17 22:04:03 +0600

    Enverment setup & Database schema writen

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-const express    = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.get("/", (req, res) => {
+	res.send("<h2> Yes I'm working good. </h2>");
+});
 /* port define */
 const _port = process.env.PORT || 4000;
-app.listen(_port, ()=>{
+app.listen(_port, () => {
 	console.log(`Application listening on port: ${_port}`);
 });
